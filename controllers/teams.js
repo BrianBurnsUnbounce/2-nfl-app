c456d5375280675ddf8f9fafdf4432bbd6c712fe
Brian Burns <brian.burns@unbounce.com>
2016-10-08 08:34:40 -0700

    team response working

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,31 +1,39 @@
 var express = require('express')
-    , router = express.Router();
+    , router = express.Router()
+    , http = require('http')
+    , request = require('request')
+    , xml2js = require('xml2js');
 
 
-// var baseUrl = 'http://www.fantasyfootballnerd.com/service/';
-// var apiKey = '7hw378dceajv';
+var baseUrl = 'http://www.fantasyfootballnerd.com/service/';
+var apiKey = '7hw378dceajv';
 
 /**
  * GET /teams
  * Get the list of NFL teams
  * URL Structure: /service/{SERVICE-NAME}/{FORMAT}/{API-KEY}/
  */
-router.get('/', function (req, res) {
+router.get('/', function (req, response) {
 
-    req.get('http://www.fantasyfootballnerd.com/service/nfl-teams/xml/test/', function (err, request, xml) {
-        if (err) return next(err);
-        parser.parseString(xml, function (err, parsedXml) {
-            if (err) return next(err);
+    var teamsUrl = baseUrl + 'nfl-teams/xml/' + apiKey;
+
+    var parser = new xml2js.Parser();
+
+    request.get(teamsUrl, function (err, res) {
+        console.log("Attempting parse");
+        parser.parseString(res.body, function (err, parsedXml) {
             try {
-                var team = parsedXml.NFLTeams.team[0].$.fullName;
+                console.log("Parsing teams...");
+                var team = parsedXml.NFLTeams.Team[0].$.fullName;
+                console.log(team);
+                response.send(team);
             } catch (err) {
-                return res.status(400).send({ message: 'XML Parse Error' });
+                console.log("error");
+                return response.status(400).send({ message: 'XML Parse Error' });
             }
-        })
+        });
+
     });
-
-    res.render('teams');
-
 });
 
 module.exports = router;
